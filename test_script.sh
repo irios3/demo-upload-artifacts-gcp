da23c4441447d3ccbe161871858121416622f271
irios3 <84444198+irios3@users.noreply.github.com>
2022-01-17 15:16:22 +0100

    Update test_script.sh

--- test_script.sh
+++ test_script.sh
@@ -1,4 +1,4 @@
-WARNING_COUNT=50
+WARNING_COUNT=150
 METRICS_FILE="metrics.txt"
 #echo warnings $WARNING_COUNT
 #echo $WARNING_COUNT
@@ -6,6 +6,7 @@ THRESHOLD=100
 
 if [ $WARNING_COUNT -ge $THRESHOLD ]; then
 	echo "Number of warnings is $WARNING_COUNT. This is greater than threshold $THRESHOLD"
+	exit 1
 else
 	echo "Number of warnings is $WARNING_COUNT. This is lower than threshold $THRESHOLD"
 fi
