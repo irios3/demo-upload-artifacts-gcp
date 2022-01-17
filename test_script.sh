cfa267a61bdad80bdc8440da088eaf5f71529a5c
irios3 <84444198+irios3@users.noreply.github.com>
2022-01-17 13:14:55 +0100

    Update test_script.sh

--- test_script.sh
+++ test_script.sh
@@ -2,6 +2,7 @@ WARNING_COUNT=50
 METRICS_FILE="metrics.txt"
 #echo warnings $WARNING_COUNT
 #echo $WARNING_COUNT
+THRESHOLD=100
 
 if [ $WARNING_COUNT \> $THRESHOLD ]; then
 	echo "Number of warnings is $WARNING_COUNT. This is greater than threshold $THRESHOLD "
