15895ad64c608f4dd5052ee65c87c2a3a9f491c0
irios3 <84444198+irios3@users.noreply.github.com>
2022-01-17 15:27:31 +0100

    Update test_script.sh

--- test_script.sh
+++ test_script.sh
@@ -6,6 +6,7 @@ THRESHOLD=100
 
 if [ $WARNING_COUNT -ge $THRESHOLD ]; then
 	echo "Number of warnings is $WARNING_COUNT. This is greater than threshold $THRESHOLD"
+	exit 1
 else
 	echo "Number of warnings is $WARNING_COUNT. This is lower than threshold $THRESHOLD"
 fi
